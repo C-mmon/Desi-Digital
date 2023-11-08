651d36c4528f7fc53bc555d14a38266a9123ddbe
C-mmon <aniketsinha101@gmail.com>
2023-11-08 23:54:59 +0530

    simplify stack

---
+++ Leetcode/simply_Directory.py
@@ -0,0 +1,15 @@
+      vector<string> dirOrFiles;
+        stringstream ss(path);
+        string dirOrFile;
+        while (getline(ss, dirOrFile, '/')) {
+            if (!dirOrFiles.empty() && dirOrFile == "..") {
+                dirOrFiles.pop_back();
+            } else if (dirOrFile != "." && dirOrFile != "" && dirOrFile != "..") {
+                dirOrFiles.push_back(dirOrFile);
+            }
+        }
+        string simplified_path = "";
+        for (string dirOrFile : dirOrFiles) {
+            simplified_path += "/" + dirOrFile;
+        }
+        return simplified_path.empty() ? "/" : simplified_path;
